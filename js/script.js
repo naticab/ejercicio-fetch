04837859202a2d6a5b8f85d07cc75a661b9dd27d
Natalia Cabrera <cabreranatalia3010@gmail.com>
2024-09-02 16:32:46 -0300

    Realizo fetch

--- js/script.js
+++ js/script.js
@@ -19,3 +19,33 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+function getJSONData(url) {
+  let result = {};
+  return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw Error(response.statusText);
+      }
+    })
+    .then(function (response) {
+      result.status = 'ok';
+      result.data = response;
+      return result;
+    })
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      return result;
+    });
+}
+
+getJSONData(DATA_URL).then((result) => {
+  if (result.status === "ok") {
+    let personalData = result.data.students;
+    showData(personalData);
+  } else {
+    alert("Error.");
+  }
+})
